a89ecc9cd5e16ea7284213b1032045d9c0b486b1
Ligaga <asergy34@gmail.com>
2018-02-23 22:44:14 +0300

    Изменил хост для БД6

--- api/db_handler.js
+++ api/db_handler.js
@@ -2,11 +2,11 @@
 var mssql = require('mssql'); 
 
 var config = {
-    user : process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',					                        // пользователь базы данных
-    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || '',			                        // пароль пользователя
+    user : process.env.OPENSHIFT_MYSQL_DB_USERNAME,			// пользователь базы данных
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,		// пароль пользователя
     database : 'lidb',				                        // имя бд
-    server : process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',				                        // хост
-    port : process.env.OPENSHIFT_MYSQL_DB_PORT || '3306',						                    // порт, на котором запущен sql server
+    server : process.env.OPENSHIFT_MYSQL_DB_HOST,			// хост
+    port : process.env.OPENSHIFT_MYSQL_DB_PORT || '3306',	// порт, на котором запущен sql server
     pool: {
         max: 10, 						                    // максимальное допустимое количество соединений пула 
         min: 0,  						                    // минимальное допустимое количество соединений пула 
--- api/db_session.js
+++ api/db_session.js
@@ -6,10 +6,10 @@ var MSSQLStore = require('connect-mssql')(session);
 var mssql = require('mssql');
 
 var config = {
-	user: process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',                   // пользователь базы данных
-	password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD || '', 	        // пароль пользователя 
-	server: process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',                // хост
-	database: 'lidb',               // имя бд
+	user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,               // пользователь базы данных
+	password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD, 	         // пароль пользователя 
+	server: process.env.OPENSHIFT_MYSQL_DB_HOST ,                // хост
+	database: 'lidb',                                            // имя бд
 	port: process.env.OPENSHIFT_MYSQL_DB_PORT || '3306',			            // порт, на котором запущен sql server
 	pool: {
         max: 10,                   // максимальное допустимое количество соединений пула 
