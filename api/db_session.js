d3908fec0e48cfcc94cbbd70ea6ba080475597e5
Ligaga <asergy34@gmail.com>
2018-02-23 22:19:52 +0300

    Изменил хост для БД6

--- api/db_session.js
+++ api/db_session.js
@@ -6,13 +6,13 @@ var MSSQLStore = require('connect-mssql')(session);
 var mssql = require('mssql');
 
 var config = {
-	user: 'user',                   // пользователь базы данных
-	password: 'SAP1428', 	        // пароль пользователя 
-	server: process.env.OPENSHIFT_MYSQL_DB_URL,                // хост
+	user: process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'root',                   // пользователь базы данных
+	password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD || '', 	        // пароль пользователя 
+	server: process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',                // хост
 	database: 'lidb',               // имя бд
-	port: 3306,			            // порт, на котором запущен sql server
+	port: process.env.OPENSHIFT_MYSQL_DB_PORT || '3306',			            // порт, на котором запущен sql server
 	pool: {
-        max: 100,                   // максимальное допустимое количество соединений пула 
+        max: 10,                   // максимальное допустимое количество соединений пула 
         min: 0,                     // минимальное допустимое количество соединений пула 
         idleTimeoutMillis: 30000    // время ожидания перед завершением неиспользуемого соединения 
     }	 
